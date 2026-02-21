ec88a74702c16aec28d0fbd29d8cd47a2cb3ae59
hipokito <hipokito@example.com>
2026-02-21 22:08:14 +0000

    Updates.

--- libs.sh
+++ libs.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
 
-VERSION="v2.8.0"
-URL="https://github.com/datso/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
+PJSIP_VERSION="2.16"
+PJSIP_URL="https://github.com/pjsip/pjproject/archive/refs/tags/${PJSIP_VERSION}.tar.gz"
+PJSIP_DIR="pjproject-${PJSIP_VERSION}"
 LOCK=".libs.lock"
-DEST=".libs.tar.gz"
-DOWNLOAD=true
+NDK_PATH="$HOME/Android/Sdk/ndk/27.1.12297006"  # your NDK from earlier
 
-if ! type "curl" > /dev/null; then
-    echo "Missed curl dependency" >&2;
-    exit 1;
-fi
 if ! type "tar" > /dev/null; then
     echo "Missed tar dependency" >&2;
     exit 1;
@@ -18,16 +14,36 @@ fi
 
 if [ -f ${LOCK} ]; then
     CURRENT_VERSION=$(cat ${LOCK})
-
-    if [ "${CURRENT_VERSION}" == "${VERSION}" ];then
-        DOWNLOAD=false
+    if [ "${CURRENT_VERSION}" == "${PJSIP_VERSION}" ]; then
+        echo "PJSIP ${PJSIP_VERSION} already built"
+        exit 0
     fi
 fi
 
-if [ "$DOWNLOAD" = true ]; then
-    curl -L --silent "${URL}" -o "${DEST}"
-    tar -xvf "${DEST}"
-    rm -f "${DEST}"
+# Download and extract source
+wget ${PJSIP_URL} -O pjsip.tar.gz
+tar -xzf pjsip.tar.gz
+rm pjsip.tar.gz
+cd ${PJSIP_DIR}
 
-    echo "${VERSION}" > ${LOCK}
-fi
+# Configure for Android
+export ANDROID_NDK_ROOT=${NDK_PATH}
+./configure-android --use-ndk-cflags --min-api=24 --openssl-version=3.3 --disable-libyuv --disable-libwebrtc --disable-video
+
+# Build
+make dep
+make
+
+# Copy libs to jni/libs
+cp pjsip/lib/*.a ../libs/
+cp pjlib/lib/*.a ../libs/
+cp pjlib-util/lib/*.a ../libs/
+cp pjmedia/lib/*.a ../libs/
+cp pjnath/lib/*.a ../libs/
+cp pjlib/bin/*.a ../libs/  # if any
+
+cd ..
+rm -rf ${PJSIP_DIR}
+echo "${PJSIP_VERSION}" > ${LOCK}
+
+echo "PJSIP ${PJSIP_VERSION} built and libs copied"
